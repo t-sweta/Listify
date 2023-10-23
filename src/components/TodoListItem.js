27a719504f0633e6c748bc5075acb9f8b11d5c8f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-10-23 18:08:16 +0530

    Updated Code

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,9 +1,7 @@
 import React from "react"
 
-export default function TodoListItem(props){
-    // let key=props.key;
-    // let item=props.item;
-    return(
+export default function TodoListItem(props) {
+    return (
         <li key={props.keys}>{props.task}</li>
     )
 }
