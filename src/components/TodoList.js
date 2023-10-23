2b5d26e94db65c112880834b889d7c1bf6eb8978
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-10-23 17:43:14 +0530

    TodoList.js Updated with HTML

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,12 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
+import TodoListItem from "./TodoListItem";
 
 function TodoList() {
-  const[items, setItems] = useState([]);
-  const[newItems, setNewItems] = useState("");
-  const addItem = () =>{
-    if(newItems){
-      setItems([...items,newItems])
+  const [items, setItems] = useState([]);
+  const [newItems, setNewItems] = useState("");
+  const addItem = () => {
+    if (newItems) {
+      setItems([...items, newItems])
       setNewItems("")
     }
   }
@@ -15,14 +15,18 @@ function TodoList() {
     <div>
       <h1>Todo List</h1>
       <input type="text"
-      value={newItems} 
-      onChange={(e)=>setNewItems(e.target.value)}
-      placeholdr="Add new task"
+        value={newItems}
+        onChange={(e) => setNewItems(e.target.value)}
+        placeholder="Add new task"
       />
-     
-      
+      <button onClick={addItem}>Add</button>
+      <ul>
+        {items.map((item, index) => (
+          <TodoListItem key={index} task={item} />
+        )}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default TodoList
